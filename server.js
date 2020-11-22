3487cd376e232a4806805e8de87d53a8d27e9f3e
fabioms <fabioms.comp@gmail.com>
2020-11-22 16:29:15 -0300

    cors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
+const helmet = require('helmet')
 const {check, validationResult} = require('express-validator');
 const {pool} = require('./config')
 
@@ -9,16 +11,18 @@ const {pool} = require('./config')
 // Setup express server
 
 const app = express();
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers',
-   'Origin, X-Requested-With, Content-Type, Accept, x-auth-token'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-})
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers',
+//    'Origin, X-Requested-With, Content-Type, Accept, x-auth-token'
+//   );
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   next();
+// })
 app.use(bodyParser.json({extended:false}))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(helmet())
+app.use(cors())
 
 // Routing
 // Query DB
