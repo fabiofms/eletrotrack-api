6a67aa5b9882f9af0f2064b56bc140ff4389af8a
fabioms <fabioms.comp@gmail.com>
2020-11-22 16:55:00 -0300

    postgres ssl fixing

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = 'postgres://vjharotrqntzui:f3ffb688430cdcb99b685ee272cdc858870b586e7bd8988cd997d7066dca35ee@ec2-3-216-89-250.compute-1.amazonaws.com:5432/dc0n9865s9sbp2'
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  ssl: { rejectUnauthorized: false }
 })
 
 module.exports = {pool}
